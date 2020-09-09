5236beafa2987e6651a54e7bef2db6303d99cd81
anette87 <anettem.rivera@gmail.com>
2020-09-08 19:59:13 -0700

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,38 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value, 
+    }) 
+  }
+
+  remainingChar = () => {
+    return this.props.maxChars - this.state.message.length;
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={e => this.handleChange(e)} />
+        <p>Remaining characters:{this.remainingChar()}</p>
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+// This component takes one prop: maxChars which is a number — the maximum amount of characters a message can have. This prop is being passed in from the App component with the value 280.
+
+// You'll find an <input type="text"> in this component. Make this a controlled component by adding the attributes to the <input> element. Its value should be saved in the component's state and should update on every change to the input.
+
+// Show the remaining characters in the component. It doesn't matter how you render it, as long as the number is correct. No need to guard against input that is too long — you can let the counter reach negative values.
